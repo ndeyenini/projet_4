f8ba8a581a1d89e5386e93ae5f9e05104199b721
ndeye nini senghor <sendeyenini@gmail.com>
2023-07-24 10:23:02 +0000

    modifier

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = ({heroapi:{title, subtitle, btntext, img, sociallinks, videos}}) =>
     <>
       <div className='relative h-auto w-auto flex flex-col'>
         <div className='bg-theme clip-path h-[85vh] lg:h-[75vh] md:h-[65vh] sm:h-[55vh] w-auto absolute top-0 left-0 right-0 opacity-100 z-10'></div>
-        <div className='relative opacity-100 z-20 grid item-center justify-item-center nike-container'>
+        <div className='relative opacity-100 z-20 grid items-center justify-items-center nike-container'>
           <div className='grid items-center justify-items-center mt-28 md:mt-24'>
             <h1 className='text-6xl lg:text-5xl md:text-4xl sm:text-3xl xsm:text-2xl font-extrabold filter drop-shadow-sm text-slate-200'>
               {title}
@@ -14,7 +14,7 @@ const Hero = ({heroapi:{title, subtitle, btntext, img, sociallinks, videos}}) =>
             <h1 className='text-6xl lg:text-5xl md:text-4xl sm:text-3xl xsm:text-2xl font-extrabold filter drop-shadow-sm text-slate-200'>
               {subtitle}
             </h1>
-            <button type='button' className='button-theme bg-slate-200 shadow-slate rounded-xl my-5'>{btntext}</button>
+            <button type='button' className='button-theme bg-slate-200 shadow-slate-200 rounded-xl my-5'>{btntext}</button>
             <div className='grid items-center gap-5 absolute top-[33vh] lg:top-[27vh] left-[11%] xl:left-0 w-auto h-auto'>
               {videos?.map((val, i) =>(
                 <Clips key={i}  imgsrc={val.imgsrc} clip={val.clip} />
--- src/components/utils/Item.jsx
+++ src/components/utils/Item.jsx
@@ -46,6 +46,7 @@ const onCartToggle =()=>{
         <p className='text-slate-200 filter drop-shadow text-base md:text-sm font-normal'>
           {text}
         </p>
+
         <div className='flex items-center justify-between w-28 my-2'>
           <div className='flex items-center bg-white/80 px-1 rounded'>
             <h1 className='text-black text-sm font-medium'>${price}</h1>
@@ -54,21 +55,27 @@ const onCartToggle =()=>{
             <StarIcon className='icon-style w-5 h-5 md:w-4 md:h-4' />
             <h1 className='md:text-sm font-normal text-slate-100'>{rating}</h1>
           </div>
-          <div className='flex items-center gap-3'>
-            <button
-              type='button'
-              className='bg-white/90 blur-effect-theme button-theme p-0.5 shadow shadow-sky-200' onClick={()=>onAddToCart()}
-            >
-              <ShoppingBagIcon className='icon-style text-slate-900' />
-            </button>
-            <button
-              type='button'
-              className='bg-white/90 blur-effect-theme button-theme px-2  py-1 shadow shadow-sky-200 text-sm text-black  'onClick={()=>{onAddToCart(); onCartToggle()}}
-            >
-              {btn}
-            </button>
-          </div>
         </div>
+        <div className='flex items-center gap-3'>
+          <button
+            type='button'
+            className='bg-white/90 blur-effect-theme button-theme p-0.5 shadow shadow-sky-200'
+            onClick={() => onAddToCart()}
+          >
+            <ShoppingBagIcon className='icon-style text-slate-900' />
+          </button>
+          <button
+            type='button'
+            className='bg-white/90 blur-effect-theme button-theme px-2  py-1 shadow shadow-sky-200 text-sm text-black  '
+            onClick={() => {
+              onAddToCart();
+              onCartToggle();
+            }}
+          >
+            {btn}
+          </button>
+        </div>
+
         <div
           className={`flex items-center ${
             ifExists ? "absolute top-5 right-1" : "justify-center"
